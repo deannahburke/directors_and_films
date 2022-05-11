630077cd5643ec68ba8a7fc32b6897af2697ac5f
Deannah Burke <deannah@bierstadtlager.com>
2022-05-11 17:00:21 -0600

    Add shoulda-matchers, get model tests to pass

--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-# RSpec.describe Director do
-#   describe 'relationships' do
-#     it {should have_many :films}
-#   end
-# end
+RSpec.describe Director do
+  describe 'relationships' do
+    it {should have_many :films}
+  end
+end
--- spec/models/film_spec.rb
+++ spec/models/film_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-# RSpec.describe Film do
-#   describe 'relationships' do
-#     it {should belong_to :director}
-#   end
-# end
+RSpec.describe Film do
+  describe 'relationships' do
+    it {should belong_to :director}
+  end
+end
