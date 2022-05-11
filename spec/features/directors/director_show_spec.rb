f182d36ef7436ae7d22ac86414dbaa7500f82067
Deannah Burke <deannah@bierstadtlager.com>
2022-05-11 15:36:49 -0600

    Add feature test for User Story 1, index feature

--- spec/features/directors/director_show_spec.rb
+++ spec/features/directors/director_show_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'the directors show page' do
+RSpec.describe 'the directors show page' do  
   # As a visitor
   # When I visit '/parents/:id'
   # Then I see the parent with that id including the parent's attributes:
   # - data from each column that is on the parent table
   it 'displays the director id and attributes' do
-    director = Director.create!(name: 'Jordan Peele', birthdate: '1979-02-21', hometown: 'New York', active: true, imdb_rating: 16)
+    director = Director.create(name: 'Jordan Peele', birthdate: '1979-02-21', hometown: 'New York', active: true, imdb_rating: 16)
     visit "/directors/#{director.id}"
 
     expect(page).to have_content(director.name)
+    expect(page).to have_content(director.birthdate)
+    expect(page).to have_content(director.hometown)
+    expect(page).to have_content(director.active)
+    expect(page).to have_content(director.imdb_rating)
   end
 end
