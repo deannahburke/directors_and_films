a47461de33adb540e283f1d836bc6c4c57c069d2
Deannah Burke <deannah@bierstadtlager.com>
2022-05-13 20:09:59 -0600

    Write first test and method for user story 6

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,4 +2,9 @@ class Director < ApplicationRecord
   has_many :films
   validates_presence_of :name, :birthdate, :hometown, :imdb_rating
   validates :active, inclusion: [true, false]
+
+  def self.most_recently_created
+    order(created_at: :desc)
+  end
+
 end
--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Director, type: :model do
     it { should validate_presence_of :imdb_rating }
     it { should allow_value(true).for(:active) }
   end
+
+  describe 'instance methods' do
+    describe '#most_recently_created' do
+        it 'returns Directors index sorted by most recently created' do
+            director_1 = Director.create!(name: 'Luke Swenson', birthdate: '1979-02-21', hometown: 'New York', active: true, imdb_rating: 16)
+
+            director_2 = Director.create!(name: 'Chloe Zhao', birthdate: '1982-03-31', hometown: 'Bejing', active: true, imdb_rating: 20)
+
+            director_3 = Director.create!(name: 'Alfred Hitchcock', birthdate: '1899-08-13', hometown: 'London', active: false, imdb_rating: 3)
+
+        expect(Director.most_recently_created.to_a).to eq([director_3, director_2, director_1])
+      end
+    end
+  end
 end
