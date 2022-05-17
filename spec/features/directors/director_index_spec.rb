c242658319cb0f6596a187292629e361d07c0e20
Deannah Burke <deannah@bierstadtlager.com>
2022-05-17 12:04:15 -0600

    Finish user story 17, add update link to director index

--- spec/features/directors/director_index_spec.rb
+++ spec/features/directors/director_index_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'the directors index page', type: :feature do
+
   # For each parent table
   # As a visitor
   # When I visit '/parents'
@@ -22,6 +23,7 @@ RSpec.describe 'the directors index page', type: :feature do
 
     expect(page).to have_content(director.created_at)
   end
+
   # As a visitor
   # When I visit any page on the site
   # Then I see a link at the top of the page that takes me to the Child Index
@@ -32,6 +34,7 @@ RSpec.describe 'the directors index page', type: :feature do
 
     expect(current_path).to eq("/films")
   end
+
   # As a visitor
   # When I visit any page on the site
   # Then I see a link at the top of the page that takes me to the Parent Index
@@ -41,5 +44,20 @@ RSpec.describe 'the directors index page', type: :feature do
     click_on "Director Index"
 
     expect(current_path).to eq("/directors")
+  end
+
+  # As a visitor
+  # When I visit the parent index page
+  # Next to every parent, I see a link to edit that parent's info
+  # When I click the link
+  # I should be taken to that parents edit page where I can update its information just like in User Story 4
+  it 'links to the edit page' do
+    director = Director.create!(name: 'Jordan Peele', birthdate: '1979-02-21', hometown: 'New York', active: true, imdb_rating: 16)
+
+    visit "/directors"
+
+    click_link "Edit #{director.name}"
+
+    expect(current_path).to eq("/directors/#{director.id}/edit")
   end
 end
